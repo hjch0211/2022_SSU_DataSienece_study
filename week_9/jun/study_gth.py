f01e069fca1ae8d898e1e6bb1112e9f8a6c63aa2
jungeol-h <dali2728@gmail.com>
2022-05-06 22:48:36 +0900

    fix errors the issue of pairplot is soloved by changing dtype

--- week_9/jun/study_gth.py
+++ week_9/jun/study_gth.py
@@ -12,7 +12,7 @@ titanic.to_csv('C:/Users/dali1/OneDrive - 숭실대학교 - Soongsil University/
 import matplotlib.pyplot as plt # 차트 그리기
 
 # 결측값 채우기
-"""
+
 print(titanic.isnull().sum())
 titanic['age'] = titanic['age'].fillna(titanic['age'].median) # 중앙값으로 age 결측값 채우기
 titanic['embarked'] = titanic['embarked'].fillna('S') #최빈값 S로 결측값 채우기 value_counts()로 확인할 수 있음
@@ -20,7 +20,7 @@ titanic['embark_town'] = titanic['embark_town'].fillna('Southampton') #최빈값
 titanic['deck'] = titanic['deck'].fillna('C') # 최빈값으로 deck 채우기... value_counts()로 확인할 수 있음
 print(titanic.info())
 print(titanic['survived'].value_counts())   ## titanic.survived 의 형식으로도 가능
-"""
+
 
 # 남자 승객과 여자 승객의 생존율 pie 차트로 그리기
 """f,ax = plt.subplots(1,2, figsize=(10,5)) # 1행 2열짜리 만듦 .ax 는 각 축
@@ -49,13 +49,15 @@ titanic_corr.to_csv('C:/Users/dali1/OneDrive - 숭실대학교 - Soongsil Univer
 """
 print(titanic['survived'].corr(titanic['adult_male'])) ##생존과 성인 남자와의 관계. -0.5577
 print(titanic['survived'].corr(titanic['fare']))
-"""
 
+"""
 #상관 분석 시각화하기
-"""
-sns.pairplot(titanic, hue = 'survived')
+titanic['adult_male'] = titanic['adult_male'].astype(int) ##pariplot에서 bool값을 못읽음
+titanic['alone'] = titanic['alone'].astype(int) ##pariplot에서 bool값을 못읽음
+sns.pairplot(titanic, hue= 'survived')
+plt.title('test')
 plt.show()
-"""
+
 
 #두 변수만 상관분석 시각화
 """
@@ -64,8 +66,10 @@ plt.show()
 """
 
 #변수 사이의 상관계수를 히트맵으로 표현. 교수님 방식
+"""
 sns.heatmap(titanic.corr(method ='pearson'), annot = True)
 plt.show()
+"""
 
 
 
