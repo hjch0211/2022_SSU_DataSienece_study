32e5a6ec3690f7aa312951a1f437cb3cdd62ef7d
Jchan <gkswocks123@gmail.com>
2022-06-03 23:57:11 +0900

    개발 너무 싫어..

--- 기말프로젝트/jchan.py
+++ 기말프로젝트/jchan.py
@@ -8,39 +8,38 @@ import requests
 class CommercialArea: 
     decode_serviceKey = 'NwS7t35ZvejHIExKbCymXgmyoLk0WLkZKDslnArD0xyBdUm3ohgWtefXVg25j9L9z3UdyDXXWbfIVjwGfOFArw=='    
     ednode_serviceKey = 'NwS7t35ZvejHIExKbCymXgmyoLk0WLkZKDslnArD0xyBdUm3ohgWtefXVg25j9L9z3UdyDXXWbfIVjwGfOFArw%3D%3D'
-    
-    ### getDivId(구코드, 찾을 행정동)
-    def getDivId(self, signguCd, adongNm):
-        #signguCd = '11590' 동작구
-        webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi")
-        time.sleep(2) #임시 비동기 처리
-        
-        url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi?resId=dong&catId=admi'+'&serviceKey=' + self.ednode_serviceKey + '&signguCd=' + signguCd + '&type=json'
-        
-        response_json = requests.get(url, allow_redirects=False)
-        response = json.loads(response_json.content)
-        responseArr = response.get('body').get('items')
-        
-        _return = ''
-        for list in responseArr:
-            if str(list.get('adongNm')) == adongNm :
-                _return = list.get('adongCd')
-                
-            if(_return == ''):
-                _return = 'Not found'
-        
-        return _return
-        
         
     ### getCommercialArea(행정동코드)
-    def getCommercialArea(self, adongCd):
+    def getCommercialArea(self, signguCd, adongNm):
+        ### getDivId(구코드, 찾을 행정동)
+        def getDivId():
+            #signguCd = '11590' 동작구
+            webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi")
+            time.sleep(2) #임시 비동기 처리
+            
+            url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi?resId=dong&catId=admi'+'&serviceKey=' + self.ednode_serviceKey + '&signguCd=' + signguCd + '&type=json'
+            
+            response_json = requests.get(url, allow_redirects=False)
+            response = json.loads(response_json.content)
+            responseArr = response.get('body').get('items')
+            
+            _return = ''
+            for list in responseArr:
+                if str(list.get('adongNm')) == adongNm :
+                    _return = list.get('adongCd')
+                    
+                if(_return == ''):
+                    _return = 'Not found'
+            
+            return _return
+        
         webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong")
         time.sleep(2) #임시 비동기 처리
         
         #params
         url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong'
         divId='adongCd'
-        key = adongCd
+        key = getDivId()
         params ={'serviceKey' : self.decode_serviceKey, 'pageNo' : '1', 'numOfRows' : '1000', 'divId' : divId, 'key' : key, 'type' : 'json' }
         
         response_json = requests.get(url, params=params, allow_redirects=False)
@@ -63,6 +62,5 @@ class CommercialArea:
         
 ##이런식으로 사용하면 됨
 #test = CommercialArea()
-#DivId = (test.getDivId('11590', '상도1동'))
-#print(test.getCommercialArea(DivId))
+#print(test.getCommercialArea('11590', '상도1동')
 ##리펙토링 마렵네요..
--- 기말프로젝트/main.py
+++ 기말프로젝트/main.py
@@ -4,5 +4,7 @@ Created on Fri May 27 22:00:32 2022
 
 @author: dali1
 """
+from jchan import CommercialArea
 
-
+test = CommercialArea()
+print(test.getCommercialArea('11590', '상도1동'))
