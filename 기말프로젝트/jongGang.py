5d6d7c7c55c3ae7129185c39cc5811b4582f50ff
Jchan <gkswocks123@gmail.com>
2022-06-13 11:58:35 +0900

    내 파일에러떠서 main처럼 사용해야할 듯

--- 기말프로젝트/jongGang.py
+++ 기말프로젝트/jongGang.py
@@ -4,47 +4,44 @@ import webbrowser
 import time
 import requests
 
-decode_serviceKey = 'NwS7t35ZvejHIExKbCymXgmyoLk0WLkZKDslnArD0xyBdUm3ohgWtefXVg25j9L9z3UdyDXXWbfIVjwGfOFArw=='    
-ednode_serviceKey = 'NwS7t35ZvejHIExKbCymXgmyoLk0WLkZKDslnArD0xyBdUm3ohgWtefXVg25j9L9z3UdyDXXWbfIVjwGfOFArw%3D%3D'
+decode_serviceKey = 'g9uyVk1xMZNy1G+bBKcWE6O0ywsy+3Om+NA/2B+KUvULR+Dg9uiN+z5S+XFoFnGQOE22ooo5GHovHau0HpNW7w=='    
+ednode_serviceKey = 'g9uyVk1xMZNy1G%2BbBKcWE6O0ywsy%2B3Om%2BNA%2F2B%2BKUvULR%2BDg9uiN%2Bz5S%2BXFoFnGQOE22ooo5GHovHau0HpNW7w%3D%3D'
 
-### getCommercialArea(행정동코드)
-def getCommercialArea(signguCd, adongNm):
-    ### getDivId(구코드, 찾을 행정동)
-    def getDivId():
-        #signguCd = '11590' 동작구
-        webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi")
-        time.sleep(1.5) #임시 비동기 처리
+#1159053000
+def getDivId(signguCd,adongNm):
+    #signguCd = '11590' 동작구
+    webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi")
+    time.sleep(1.5) #임시 비동기 처리
             
-        url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi?resId=dong&catId=admi'+'&serviceKey=' + ednode_serviceKey + '&signguCd=' + signguCd + '&type=json'
-            
-        response_json = requests.get(url, allow_redirects=False)
-        response = json.loads(response_json.content)
-        responseArr = response.get('body').get('items')
-            
-        _return = ''
-        for list in responseArr:
-            if str(list.get('adongNm')) == adongNm :
-                _return = list.get('adongCd')
-                
-            if(_return == ''):
-                _return = 'Not found'
-            
-        return _return
+    url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi?resId=dong&catId=admi'+'&serviceKey=' + ednode_serviceKey + '&signguCd=' + signguCd + '&type=json'
         
-    webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong")
-    time.sleep(2) #임시 비동기 처리
+    response_json = requests.get(url, allow_redirects=False)
+    response = json.loads(response_json.content)
+    responseArr = response.get('body').get('items')
+            
+    _return = ''
+    for list in responseArr:
+        if str(list.get('adongNm')) == adongNm :
+            _return = list.get('adongCd')
+                
+        if(_return == ''):
+            _return = 'Not found'
+            
+    return _return
+
+def getCommercialArea(signguCd, adongNm):
+    webbrowser.open('http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong')
+    time.sleep(1.5) #임시 비동기 처리
         
     #params
     url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong'
-    divId='adongCd'
-    key = getDivId()
+    divId = 'adongCd'
+    key = str(getDivId(signguCd,adongNm))
     params ={'serviceKey' : decode_serviceKey, 'pageNo' : '1', 'numOfRows' : '1000', 'divId' : divId, 'key' : key, 'type' : 'json' }
         
     response_json = requests.get(url, params=params, allow_redirects=False)
     response = json.loads(response_json.content)
     responseArr = response.get('body').get('items')
-
-    _return = pd.DataFrame(responseArr)
         
     #테스트 필요
     page = response.get('body').get('totalCount') // 1000 + 1
@@ -56,7 +53,10 @@ def getCommercialArea(signguCd, adongNm):
             add_responseArr = add_response.get('body').get('items')
             responseArr = responseArr + add_responseArr
             
+    _return = pd.DataFrame(responseArr)
+            
     return _return
+    
     
 ### 필요한 인자가 시군구코드, 찾을행정동
 class CommercialArea:
@@ -67,7 +67,8 @@ class CommercialArea:
         
     def getDf(self):
         return self._data
-##이런식으로 사용하면 됨
-#test = CommercialArea('11590', '상도1동')
-#test.getDf()
-##리펙토링 마렵네요..
+    
+    def getCoordinate(self):
+        hospital = self._data[['indsMclsNm']=='병원']
+        cinema = self._data[['indsSclsNm']=='영화관']
+        store = self._data[['indsSclsNm']=='백화점']
