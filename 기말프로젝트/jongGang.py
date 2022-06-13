c8a12956f47a7549acfa342a2d58ac3fdb820149
Jchan <gkswocks123@gmail.com>
2022-06-13 14:31:48 +0900

    또 잘됨. 쥬피터노트북 리로딩 적용이 엄청느린듯

--- 기말프로젝트/jongGang.py
+++ 기말프로젝트/jongGang.py
@@ -9,10 +9,7 @@ ednode_serviceKey = 'g9uyVk1xMZNy1G%2BbBKcWE6O0ywsy%2B3Om%2BNA%2F2B%2BKUvULR%2BD
 
 #1159053000
 def getDivId(signguCd,adongNm):
-    #signguCd = '11590' 동작구
-    webbrowser.open("http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi")
-    time.sleep(1.5) #임시 비동기 처리
-            
+    #signguCd = '11590' 동작구     
     url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/baroApi?resId=dong&catId=admi'+'&serviceKey=' + ednode_serviceKey + '&signguCd=' + signguCd + '&type=json'
         
     response_json = requests.get(url, allow_redirects=False)
@@ -30,8 +27,8 @@ def getDivId(signguCd,adongNm):
     return _return
 
 def getCommercialArea(signguCd, adongNm):
-    webbrowser.open('http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong')
-    time.sleep(1.5) #임시 비동기 처리
+#     webbrowser.open('http://apis.data.go.kr/B553077/api/open/sdsc2')
+#     time.sleep(1.5) #임시 비동기 처리
         
     #params
     url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong'
@@ -56,7 +53,28 @@ def getCommercialArea(signguCd, adongNm):
     _return = pd.DataFrame(responseArr)
             
     return _return
+
+### 최대반경 2km
+def getStoreListInRadius(cx, cy):
+    storeList = ['병원', '백화점', '영화관']
     
+    for i in range(1,3):
+        #params
+        url = 'http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInRadius'
+        divId = 'adongCd'
+        key = str(getDivId(signguCd,adongNm))
+        params = {'serviceKey' : decode_serviceKey, 'pageNo' : '1', 'numOfRows' : '1000', 'radius' : '2000', 'cx' : cx, 'cy' : cy, 'type' : 'json' }
+        if storeList[i] == '병원':
+            params['indsMclsCd'] = storeList[i]
+        else:
+            params['indsSclsCd'] = storeList[i]
+        
+        response_json = requests.get(url, params=params, allow_redirects=False)
+        response = json.loads(response_json.content)
+        storeList[i] = response.get('body').get('items')
+        
+    _return = pd.DataFrame(storeList)
+    return _return
     
 ### 필요한 인자가 시군구코드, 찾을행정동
 class CommercialArea:
@@ -64,11 +82,11 @@ class CommercialArea:
         self._signguCd = signguCd
         self._adongNm = adongNm
         self._data = getCommercialArea(self._signguCd, self._adongNm)
+        self.cx = "준걸이 수정하면 수정하기"
+        self.cy = ""
         
     def getDf(self):
         return self._data
     
     def getCoordinate(self):
-        hospital = self._data[['indsMclsNm']=='병원']
-        cinema = self._data[['indsSclsNm']=='영화관']
-        store = self._data[['indsSclsNm']=='백화점']
+        return getStoreListInRadius(self.cx, self.cy)
