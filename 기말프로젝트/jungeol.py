abba74ab67b54c802dcdbdd341e8491ce5b6355b
jungeol-h <dali2728@gmail.com>
2022-06-14 12:09:33 +0900

    좌표 반환 추가

--- 기말프로젝트/jungeol.py
+++ 기말프로젝트/jungeol.py
@@ -12,6 +12,11 @@ def load_data(univ):
     result_data= excel_data[excel_data['대학']==univ]
     return str(result_data['시군구코드'].values[0]), result_data['행정동이름'].values[0]
 
+def load_xy(univ):
+    excel_data = pd.read_csv("대학리스트.csv")
+    result_data= excel_data[excel_data['대학']==univ]
+    return str(result_data['경도'].values[0]), result_data['위도'].values[0]
+
     
     
 def main():
@@ -30,9 +35,13 @@ class LoadUnivData:
     def __init__(self, univ):
         self.univ = univ
         self.gucode, self.dongname = load_data(self.univ)
+        self.cx, self.cy = load_xy(self.univ)
         
         
     def loadUniv(self):
         return self.gucode, self.dongname
+    
+    def loadXY(self):
+        return self.cx, self.cy
         
         
